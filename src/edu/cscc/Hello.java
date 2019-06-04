a8af248f5f8692b2883b726dd099a95a679c2278
fogunlayi <fogunlayi@gmail.com>
2019-06-04 19:17:55 -0400

    My first commit

---
+++ src/edu/cscc/Hello.java
@@ -0,0 +1,9 @@
+package edu.cscc;
+//* Folu JAF-Ogunlayi, Lab 2 , 06/04/2019 *//
+public class Hello{
+    public static void main(String[] args) {
+        // Prints "Hello, World"
+        System.out.println("Hello, World");
+
+}
+}
